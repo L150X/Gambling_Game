781d82f2733f236a7511cdd6184477f7df3202a2
jacob <jacobdavis2a7v@gmail.com>
2026-06-12 14:41:22 -0400

    Added read_line function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use colored::*; // Import colored crate (from cargo, e.g., cargo add colored)
+
+/// Reads a line of input from the user, returning it as a string
+fn read_line() -> String { // Return type is String
+
+    // Create a mutable String to hold the input
+    let mut input: String = String::new();
+
+    std::io::stdin() 
+        .read_line(&mut input)
+        .expect("Failed to read line");
+        input // Return input
 }
+
+/// Main Function
+fn main() {
+    let input: String = read_line(); // Call read_line and store the result in input
+}
+
